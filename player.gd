d461b4d64c797a98525d5e16e9f1aaf5454a5937
LucianKennelly <119460731+LucianKennelly@users.noreply.github.com>
2024-09-06 14:37:24 -0700

    Added pixel art for Kanye as well as Kanye
    Got world barriers working, added animation tiles.

--- player.gd
+++ player.gd
@@ -1,11 +1,13 @@
 extends CharacterBody2D
-
-
+var can_move = true
+var has_bump =true
 var speed = 250
-
+var normal_speed = 0.5
+signal bump
 func _physics_process(delta):
 	player_movement(delta)
 func player_movement(delta):
+	var collision_info = move_and_collide(velocity * delta * normal_speed)
 	if Input.is_action_pressed("Up"):
 		velocity.y = -speed
 		velocity.x = 0
@@ -22,4 +24,15 @@ func player_movement(delta):
 		velocity.y = 0
 		velocity.x = 0
 	#velocity = velocity.normalized()
-	move_and_slide()
+	#if collision_info:
+		#velocity.y = 0
+		#velocity.x = 0
+		#if(collision_info.get_collider() is $Area2D/CollisionShape2D && has_bump):
+			#emit_signal("bump")
+			#has_bump = false
+	if velocity.x > 0:
+		get_node("AnimatedSprite2D").flip_h = false
+	elif velocity.x < 0:
+		$AnimatedSprite2D.flip_h = true
+		
+	move_and_collide(velocity * delta * normal_speed)
