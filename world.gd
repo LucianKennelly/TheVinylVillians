2dcd2590d412611262ddce475c0e4ea73b27283d
LucianKennelly <119460731+LucianKennelly@users.noreply.github.com>
2024-09-09 13:58:04 -0700

    Music
    a little background music I made with friends for now, probably change it to a variety of different songs for the lobby later.

--- world.gd
+++ world.gd
@@ -6,6 +6,7 @@ func _ready() -> void:
 	$player.position = Vector2(500, 500)
 	$player/AnimatedSprite2D.play("Idle")
 	Kanye.play("default")
+	$AudioStreamPlayer2D.play
 	$Kanye.position = Vector2(500, -500)
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
