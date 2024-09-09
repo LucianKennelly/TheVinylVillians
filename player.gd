55e987424f5b497c224044aab42ca370f0543838
LucianKennelly <119460731+LucianKennelly@users.noreply.github.com>
2024-09-08 18:56:12 -0700

    Big updates
    lots more graphics/animations.

--- player.gd
+++ player.gd
@@ -9,25 +9,34 @@ func _physics_process(delta):
 func player_movement(delta):
 	var collision_info = move_and_collide(velocity * delta * normal_speed)
 	if Input.is_action_pressed("Up"):
+		$AnimatedSprite2D.play("Run")
 		velocity.y = -speed
 		velocity.x = 0
 	elif Input.is_action_pressed("Down"):
+		$AnimatedSprite2D.play("Run")
 		velocity.y = speed
 		velocity.x = 0
 	elif Input.is_action_pressed("Left"):
+		$AnimatedSprite2D.play("Run")
 		velocity.x = -speed
 		velocity.y = 0
 	elif Input.is_action_pressed("Right"):
+		$AnimatedSprite2D.play("Run")
 		velocity.x = speed
 		velocity.y = 0
+	elif Input.is_action_pressed("Punch"):
+		$AnimatedSprite2D.play("Punch")
+		velocity.y = 0
+		velocity.x = 0
 	else:
+		$AnimatedSprite2D.play("Idle")
 		velocity.y = 0
 		velocity.x = 0
 	#velocity = velocity.normalized()
 	#if collision_info:
 		#velocity.y = 0
 		#velocity.x = 0
-		#if(collision_info.get_collider() is $Area2D/CollisionShape2D && has_bump):
+		#if collision_info.get_collider() is CollisionShape2D:
 			#emit_signal("bump")
 			#has_bump = false
 	if velocity.x > 0:
--- world.gd
+++ world.gd
@@ -5,6 +5,8 @@ extends Node2D
 func _ready() -> void:
 	$player.position = Vector2(500, 500)
 	$player/AnimatedSprite2D.play("Idle")
+	$Kanye/AnimatedSprite2D.play("default")
+	$Kanye.position = Vector2(500, -500)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
